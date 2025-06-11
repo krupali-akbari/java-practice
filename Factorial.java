41b629335494e357d4f95c3714d18f7b1c815b16
krupali-akbari <krupaliakbari1@gmail.com>
2025-06-11 13:33:10 +0530

    Added Factorial program

---
+++ Factorial.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Factorial
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the  n=");
+        int n=sc.nextInt();
+        
+        int fact=1;
+        
+        for(int i=1;i<=n;i++)
+        {
+            fact=fact*i;
+        }
+        
+        System.out.println("factorial n!="+fact);
+        
+        
+        
+      
+        
+        
+         
+    }
+}
