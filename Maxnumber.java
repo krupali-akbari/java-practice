4d8a0c2c8ad8033fc7510aecbbf50cb589894b0e
krupali-akbari <krupaliakbari1@gmail.com>
2025-06-11 12:00:22 +0530

    Added Maxnumber.java program

---
+++ Maxnumber.java
@@ -0,0 +1,22 @@
+ class Maxnumber
+{
+    
+    public static void main(String[] args)
+    {
+        int a=10,b=40,c=76;
+        
+        if(a>b && a>c)
+        {
+            System.out.println("a is max");
+        }
+        else if(b>c)
+        {
+            System.out.println("b is max");
+        }
+        else
+        {
+            System.out.println("c is max");
+        }
+        
+    }   
+}
