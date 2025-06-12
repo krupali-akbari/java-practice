3a13d6ca03521918a4f6dc0f1e5dca1503b6a928
krupali-akbari <krupaliakbari1@gmail.com>
2025-06-12 13:38:14 +0530

    Added summethod.java program

---
+++ Summethod.java
@@ -0,0 +1,15 @@
+public class Summethod 
+{
+   static int  add(int a , int b)
+   {
+     return a+b;
+   } 
+
+   public static void main(String[] args)
+{
+    
+    int x=20,y=40;
+    int result = add(x, y);
+    System.out.println("sum="+ result);
+   }
+}
